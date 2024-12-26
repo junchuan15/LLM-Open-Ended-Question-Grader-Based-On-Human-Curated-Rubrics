ad48259b21746b9a6089175328a09f4a5fe77fbc
JC <jcquah2@gmail.com>
2024-12-26 13:40:24 +0800

    user manual page done

--- pages/user_manual.py
+++ pages/user_manual.py
@@ -1,20 +1,29 @@
 import streamlit as st
 import os 
 from src import loader
-import base64
 
 # --- Load CSS ---
 css_path = os.path.join(os.path.dirname(__file__), "../styles/user_manual_page.css")
 with open(css_path) as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    
+st.markdown(
+    f"""
+    <div class="title-container">
+        <img src="data:image/png;base64,{loader.load_image(img_path='assets/logo.trans.png')}" alt="Grady Logo">
+        <div class="title">Grady - User Manual</div>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
 
-def section (num, title, desc, img):
+def section (num, header, desc, img):
     st.markdown(f"""
     <div class="section">
         <div class="number">{num}</div>
         <div class="content">
-            <div class="title">
-            {title}
+            <div class="header">
+            {header}
             </div>
             <div class="description">
                {desc}
@@ -25,9 +34,9 @@ def section (num, title, desc, img):
         <img src="data:image/png;base64,{img}" alt="Feature 1">
     </div>
 """, unsafe_allow_html=True)
-    
+
 section (1,"Create a New Assessment", 
-         "To create a new assessment, enter the course code in the input field and press 'Enter' to proceed.", 
+         "Please navigate to the 'Grading Hub' in the sidebar to start. To create a new assessment, enter the course code in the input field and press 'Enter' to proceed.", 
          loader.load_image(img_path = 'assets/step1.png'))
 
 section (2,"Upload Rubric", 
@@ -57,3 +66,24 @@ section(4, "Upload Answer",
 view_answer = st.checkbox("View Sample Answer File")
 if view_answer:
     st.markdown(loader.display_pdf("assets/sample_answer.pdf"), unsafe_allow_html=True)
+
+section(5, "Start Grading", 
+        """Once the files are uploaded, click the "Grade" button to initiate the grading process. 
+           Please note that the grading process may take a few minutes to complete, depending on the size and number of uploaded files.""", 
+        loader.load_image(img_path='assets/step5.png'))
+
+section(6, "View Grading Result", 
+        """Once the grading process is completed, the results will be displayed in a tabular format for each file, allowing users to evaluate and review the grading outcomes. 
+           Users can utilize the drop-down menu to select the specific answer they wish to view. The results include the question, marks allocation, answer matching, 
+           graded score and an explanation for the assigned score.""", 
+        loader.load_image(img_path='assets/step6.png'))
+
+section(7, "Download Result", 
+        """After reviewing the grading results, users can download the results in CSV format for their records. 
+        Click the "Download" button to save the results, which include all the evaluated details for each uploaded answer.""", 
+        loader.load_image(img_path='assets/step8.png'))
+
+section(8, "Visualize Result", 
+        """Users can navigate to the 'Visualization' tab to view the graphs and charts generated based on the overall total score results for the uploaded answers. 
+           These visualizations provide valuable insights, allowing users to analyze overall performance trends, identify patterns and evaluate the grading outcomes effectively.""", 
+        loader.load_image(img_path='assets/step8.png'))
